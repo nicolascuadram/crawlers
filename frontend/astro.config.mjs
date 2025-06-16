2d5534c370512cad6df6022509e1afb8a6752ca4
Nicolás_Cuadra <nikocuadram@gmail.com>
2025-06-15 22:16:58 -0400

    Testing deploy

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -7,7 +7,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  base: "/crawlers/",
+  outDir: "../dist",
   integrations: [mdx(), sitemap()],
 
   vite: {
